5bc34836b7f94f2e2e76a64d9c2123592cfbc7d8
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-02 23:16:30 +0000

    edit POST part

---
+++ spec/controllers/market_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
+
+describe MarketController do
+    describe 'GET market#index' do
+  
+    end
+
+
+    describe 'POST market#cart' do 
+        let!(:user1) { FactoryGirl.create(:user, username: 'joe', password_digest: '123', 'gender': 'Male', weight: '70', height: '180', age: '22', sports_level: 'Active', :id => 1) }
+        it "should give the food cost and total calories" do 
+            @request.session['current_user'] = 1
+            post :cart, {:user=>{:n1=>"3", :n2=>"2", :n3=>"3", :n4=>"1", :n5=>"1", :n6=>"1", :n7=>"1"}}
+            
+            expect(assigns(:cash)).to eql(97.99999999999999)
+            expect(assigns(:calories)).to eql(8375)
+        end
+    end
+end
+
+
