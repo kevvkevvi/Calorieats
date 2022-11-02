42ed77719d321761d56553b6f7f5210cd1b67144
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-02 16:52:05 +0000

    cucumber all green

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -62,10 +62,11 @@ When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
   find_field(field).set(value)
 end
 
+#reset market value
 When /^(?:|I )should see "([^"]*)" revert to empty$/ do |field|
   field = find_field(field)
   field_value = (field.tag_name == 'textarea') ? field.text : field.value
-  expect(field_value).to eq ""
+  expect(field_value).not_to have_content(text)
 end
 
 When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
