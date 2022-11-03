086338671cf0fe80032d233182c6cb3c4ca33c9b
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-03 00:56:59 +0000

    rspec market

--- spec/controllers/market_controller_spec.rb
+++ spec/controllers/market_controller_spec.rb
@@ -3,12 +3,11 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 describe MarketController do
-    describe 'GET market#index' do
-        let!(:user1) { FactoryGirl.create(:user, username: 'joe', password_digest: '123', 'gender': 'Male', weight: '90', height: '200', age: '19', sports_level: 'Active', :id => 1) }
-        
+    describe 'GET market#index' do      
         it 'should add food to cart' do
-            post :cart, {:user=>{:n1=>"3", :n2=>"2", :n3=>"3", :n4=>"1", :n5=>"1", :n6=>"1", :n7=>"1"}}
-            expect(response).to redirect_to(market_cart_path)
+            get :index
+            expect(response).to render_template('index')
+        end
 
     end
 
@@ -18,9 +17,9 @@ describe MarketController do
         it "should give the food cost and total calories" do 
             @request.session['current_user'] = 1
             post :cart, {:user=>{:n1=>"3", :n2=>"2", :n3=>"3", :n4=>"1", :n5=>"1", :n6=>"1", :n7=>"1"}}
-            
-            expect(assigns(:cash)).to eql(97.99999999999999)
             expect(assigns(:calories)).to eql(8375)
+            expect(assigns(:cash)).to eql(97.99999999999999)
+            
         end
     end
 end
