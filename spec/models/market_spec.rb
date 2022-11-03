88040e776ae05df51aabe28f329fcaf0ac5f2a82
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-03 02:04:19 +0000

    finish

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -8,10 +8,10 @@ SimpleCov.start 'rails'
 describe Market do
   describe '.recommended_food' do
     #let!(:user1) { FactoryGirl.create(:user, beef: '3', lamb: '2', broccoli: '3', carrot: '1', milk: '1', banana:'1', apple: '1') }
-    let!(:user1) { FactoryGirl.create(:user, username: 'joe', password_digest: '123', weight: '90', height: '200', age: '19') }
-    let!(:user2) { FactoryGirl.create(:user, username: 'jane', password_digest: '123', weight: '40', height: '165', age: '29') }
-    let!(:user3) { FactoryGirl.create(:user, username: 'john', password_digest: '123', weight: '80', height: '170',age: '21') }
-    let!(:user4) { FactoryGirl.create(:user, username: 'jim', password_digest: '123', weight: '85', height: '160', age: '28') }
+    let!(:user1) { FactoryGirl.create(:user, username: 'joe', password_digest: '123', weight: '90', height: '200', age: '19', sports_level:'Seldom (0-1 days/week)') }
+    let!(:user2) { FactoryGirl.create(:user, username: 'jane', password_digest: '123', weight: '40', height: '165', age: '29',sports_level:'Seldom (0-1 days/week)') }
+    let!(:user3) { FactoryGirl.create(:user, username: 'john', password_digest: '123', weight: '80', height: '170',age: '21',sports_level:'Seldom (0-1 days/week)') }
+    let!(:user4) { FactoryGirl.create(:user, username: 'jim', password_digest: '123', weight: '85', height: '160', age: '28',sports_level:'Seldom (0-1 days/week)') }
 
     
       #cash=user1.beef*14.99+user1.lamb*19.99+user1.broccoli*2.2+user1.carrot*0.52+user1.milk*2.65+user1.banana*0.79+user1.apple*2.49
@@ -21,21 +21,28 @@ describe Market do
 
 
     context 'Balanced diet of protein, vegetable, fruit and dairy' do
+      weight = 90
       it 'bmi is between 18.5 and 24.9' do
-        expect(bmi_result).to eql('healthy')
-        expect(Market.recommended_food(bmi_result, sports_level, weight)).to eql('Balanced diet of protein, vegetable, fruit and dairy')
+        bmi = user1.weight/(user1.height/100)**2
+        bmi_result = User.bmi_classifier(bmi, user1.age)
+        sports_level='Seldom (0-1 days/week)'
+        expect(Market.recommended_food(bmi_result,sports_level,weight)).to eql('Balanced diet of protein, vegetable, fruit and dairy')
       end
       it 'bmi is lower than 18.5' do
-        expect(bmi_result).to eql('underweight')
-        expect(Market.recommended_food(bmi_result, sports_level, weight)).to eql('More protein such as beef')
+        bmi = user2.weight/(user2.height/100)**2
+        bmi_result = User.bmi_classifier(bmi,user2.age)
+        sports_level='Seldom (0-1 days/week)'
+        expect(Market.recommended_food(bmi_result,sports_level,weight)).to eql('More protein such as beef')
       end
       it 'bmi is between 24.9 and 29.9' do
-
-        expect(bmi_result).to eql('overweight')
-        expect(Market.recommended_food(bmi_result, sports_level, weight)).to eql('More vegetable and fruit such as broccoli and banana')
+        bmi = user3.weight/(user3.height/100)**2
+        bmi_result = User.bmi_classifier(bmi, user3.age)
+        sports_level='Seldom (0-1 days/week)'
+        expect(Market.recommended_food(bmi_result,sports_level,weight)).to eql('More vegetable and fruit such as broccoli and banana')
       end
       it 'bmi is higher than 29.9' do
-        expect(bmi_result).to eql('obese')
+        bmi = user4.weight/(user4.height/100)**2
+        bmi_result = User.bmi_classifier(bmi, user4.age)
       end         
     end
   end
