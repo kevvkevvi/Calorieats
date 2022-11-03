8916f527f96a356d9af42a91c5713be17420f827
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-03 02:04:40 +0000

    finish

--- spec/controllers/market_controller_spec.rb
+++ spec/controllers/market_controller_spec.rb
@@ -16,10 +16,10 @@ describe MarketController do
         let!(:user1) { FactoryGirl.create(:user, username: 'joe', password_digest: '123', 'gender': 'Male', weight: '70', height: '180', age: '22', sports_level: 'Active', :id => 1) }
         it "should give the food cost and total calories" do 
             @request.session['current_user'] = 1
-            post :cart, {:user=>{:n1=>"3", :n2=>"2", :n3=>"3", :n4=>"1", :n5=>"1", :n6=>"1", :n7=>"1"}}
-            expect(assigns(:calories)).to eql(8375)
-            expect(assigns(:cash)).to eql(97.99999999999999)
+            post :cart, {:user=>{:number1=>3, :number2=>2, :number3=>3, :number4=>1, :number5=>1, :number6=>1, :number7=>1}}
             
+            expect(assigns(:cash)).to eql(97.99999999999999)
+            expect(assigns(:calory)).to eql(8375)
         end
     end
 end
