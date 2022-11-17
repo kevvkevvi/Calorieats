0560699b4b382ca89f35c9efb08bcaac413ddf8d
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-11-17 01:18:39 +0000

    comment out

--- spec/requests/market_spec.rb
+++ spec/requests/market_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
-require 'simplecov'
-SimpleCov.start 'rails'
+#require 'rails_helper'
+#require 'simplecov'
+#SimpleCov.start 'rails'
 
-RSpec.describe "Market", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/market/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+#RSpec.describe "Market", type: :request do
+ # describe "GET /index" do
+  #  it "returns http success" do
+   #   get "/market/index"
+    #  expect(response).to have_http_status(:success)
+    #end
+  #end
 
-end
+#end
